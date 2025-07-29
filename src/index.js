a4ae0d665f3d25cf436171dc7f50bdf15864dc1d
Olga <minaeva9@gmail.com>
2025-07-29 14:02:50 -0400

    feat: reverse(num) implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(num) {
+  if (typeof num !== 'number') return 'enter number';
+
+  const isNegative = num < 0;
+  const reversed = Math.abs(num).toString().split('').reverse().join('');
+  const result = parseInt(reversed, 10);
+
+  return isNegative ? -(-result) : result;
 };
